5587334ae21ad5bdc7b184b820112bb43fefdd06
David Kaufmann <david@kaufmann.dev>
2024-03-03 17:52:11 +0200

    fix: update testMongoDB.php

--- testMongoDB.php
+++ testMongoDB.php
@@ -4,21 +4,17 @@
     <div class="container">
         <p>
             <?php
-		echo "in php";
                 require 'vendor/autoload.php';
-		echo "require sucessful";
                 
-                // error_reporting(E_ALL ^ E_WARNING);
+                error_reporting(E_ALL ^ E_WARNING);
 
                 $servername   = $_POST["server"];
                 $database = $_POST["db"];
                 $username = $_POST["username"];
                 $password = $_POST["pw"];
-		echo "get post suicess";
 
                 // Create connection
                 $mongo = new MongoDB\Client('mongodb://' . $username . ':' . $password . '@' . $servername);
-		echo "connection suicess";
 
                 // Check connection
                 try {
@@ -32,7 +28,6 @@
                     die("<h3>Failed to connect to MongoDB</h3><samp>" . $connectionTimeoutException . "</samp>");
                 }
                 echo "<h3>Connected successfully</h3>";
-		echo "connection test suicess";
             ?>
         </p>
     </div>
